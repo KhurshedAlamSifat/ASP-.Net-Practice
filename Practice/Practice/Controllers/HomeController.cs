489ca26a9cfdf35945ebde898e3bb073e862f9a6
Your Name <kasifatmolla0@gmail.com>
2023-02-16 12:46:07 +0600

    first commit

---
+++ Practice/Practice/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Loginsubmit()
+        {
+            TempData["Msg"] = "Login Successful";
+            return RedirectToAction("Dashboard", "User");
+        }
+
+        public ActionResult Registration() 
+        {
+            return View();    
+        }
+    }
+}
