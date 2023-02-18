8229540313d8b984fea4fdfa3134a1b74bb96927
KhurshedAlamSifat <khurshedalamsifat@gmail.com>
2023-02-19 00:28:51 +0600

    EF update 2.0

--- EntityFramwork/EntityFramwork/Controllers/HomeController.cs
+++ EntityFramwork/EntityFramwork/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using EntityFramwork.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
 
 namespace EntityFramwork.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Person model) 
+        {
+            var db = new DOTNETEntities2();
+            db.People.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            var db = new DOTNETEntities2();
+            var peoples=db.People.ToList();
+            return View(peoples);
         }
 
         public ActionResult About()
