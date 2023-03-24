21d4de98352fda99f8bb49e51d8169e67af62c3c
KhurshedAlamSifat <khurshedalamsifat@gmail.com>
2023-03-24 16:44:08 +0600

    IntroAPI

---
+++ API_DataPassing/API_DataPassing/Controllers/DataPassController.cs
@@ -0,0 +1,57 @@
+﻿using API_DataPassing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_DataPassing.Controllers
+{
+    public class DataPassController : ApiController
+    {
+        [HttpGet]
+        [Route("api/students/all")]
+        public List<Student> AllStudents()
+        {
+            var students = new List<Student>();
+            for (int i = 1; i <= 5; i++)
+            {
+                students.Add(new Student()
+                {
+                    Id = i,
+                    Name = "Student " + i,
+                    Dept = "CSE"
+                });
+            }
+            return students;
+        }
+
+        [HttpGet]
+        [Route("api/students/names")]
+        public List<string> StudentsName()
+        {
+            var students = new List<Student>();
+            for (int i = 1; i <= 5; i++)
+            {
+                students.Add(new Student()
+                {
+                    Id = i,
+                    Name = "Student " + i,
+                    Dept = "CSE"
+                });
+            }
+            var names = (from s in students
+                         select s.Name).ToList();
+            return names;
+        }
+
+        [HttpGet]
+        [Route("api/manual/names")]
+        public List<string> GetStudents()
+        {
+            string[] names = new string[] { "Sifat", "Shafi", "Nadim", "Tanim", "Mugdho", "Ahsan" };
+            return names.ToList();
+        }
+    }
+}
---
+++ API_DataPassing/API_DataPassing/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_DataPassing.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        
+    }
+}
