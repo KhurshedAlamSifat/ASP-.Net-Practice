740249cb0db3681b4df9e11e2ca9c4485cda83b7
KhurshedAlamSifat <khurshedalamsifat@gmail.com>
2023-03-25 14:07:57 +0600

    CRUD API

---
+++ CRUDWithAPI/CRUDWithAPI/EF/APICRUD.cs
@@ -0,0 +1,14 @@
+﻿using CRUDWithAPI.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDWithAPI.EF
+{
+    public class APICRUD : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUDWithAPI/CRUDWithAPI/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CRUDWithAPI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CRUDWithAPI.EF.APICRUD>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CRUDWithAPI.EF.APICRUD context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
