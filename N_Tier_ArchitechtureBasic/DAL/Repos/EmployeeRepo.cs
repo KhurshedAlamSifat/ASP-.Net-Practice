11355a57fa2278b39e850ee19f8d09302bfa9d2c
KhurshedAlamSifat <khurshedalamsifat@gmail.com>
2023-04-07 14:52:40 +0600

    NTA

---
+++ N_Tier_ArchitechtureBasic/DAL/Repos/EmployeeRepo.cs
@@ -0,0 +1,50 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class EmployeeRepo
+    {
+        static EmpContext db;
+
+        static EmployeeRepo()
+        {
+            db = new EmpContext();
+        }
+        public static List<Employee> Get()
+        {
+           return db.Employees.ToList();
+          /* List<Employee> list = new List<Employee>();
+            for(int i=1; i<=10; i++)
+            {
+                list.Add(new Employee() { Id = i, Name = "Employee-" + i, Age = 30 }) ;
+            }
+            return list;*/
+        }
+        public static Employee Get(int id)
+        {
+            return db.Employees.Find(id);
+        }
+        public static bool Create(Employee employee)
+        {
+            db.Employees.Add(employee);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Update(Employee employee)
+        {
+            var ExEmployee = Get(employee.Id);
+            db.Entry(ExEmployee).CurrentValues.SetValues(employee);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Delete(int id)
+        {
+            var ExEmployee = Get(id);
+            db.Employees.Remove(ExEmployee);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
---
+++ N_Tier_ArchitechtureBasic/N_Tier_ArchitechtureBasic/Controller/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using BLL.Sevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace N_Tier_ArchitechtureBasic.Controller
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpGet]
+        [Route("api/employees")]
+        public HttpResponseMessage AllEmployees()
+        {
+            try
+            {
+                var data = EmployeeService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
